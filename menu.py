7ddc6be01219806a495c6fbd26facf61ff220fba
root <root@station101.example.com>
2019-01-10 14:13:19 +0530

    day 3 done

---
+++ menu.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env  python2
+
+import  webbrowser 
+
+options='''
+Press  1  to  check your OS version  :
+Press  2  to  login your facebook account   :
+Press  3  to check RAM and CPU in your currnet machine   :
+Press  4  to search something in google search engine :   :
+Press  5  to list out all IP and mac address in your current network zone  :
+'''
+print  options 
+#  to take input from user in python 2
+choice=raw_input()
+
+
+if    int(choice)  ==  1  :
+	print  "MY OS is RHEL "
+
+
+elif  int(choice)  ==  4  : 
+	data=raw_input("type something to search on google :  ")
+	webbrowser.open_new_tab('https://www.google.com/search?q='+data)
+
+else :
+	print  "NO valid option given "
+
+
+
+
+
+
+
